bd763d9902428abd7a50d5c6ed45bb605e5e3999
JAYBLA <jayblaenterprises@gmail.com>
2021-08-14 15:54:47 +0300

    slider

--- main/views.py
+++ main/views.py
@@ -2,5 +2,8 @@ from django.shortcuts import render, get_object_or_404, redirect, reverse
 
 def home(request):
     template_name = 'main/home.html'
-    context = {}    
+    context = {
+        'title':"Home"    
+        
+    }    
     return render(request, template_name, context)
