d4e3157e37271140f10a69ac2aecf45efd931e70
JAYBLA <jayblaenterprises@gmail.com>
2021-08-16 17:15:35 +0300

    pages

--- main/urls.py
+++ main/urls.py
@@ -6,4 +6,11 @@ app_name = 'main'
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('graphic-design', views.graphic_design, name='graphic_design'),
+    path('tshirt-printing', views.tshirt_printing, name='tshirt_printing'),
+    path('2D&3D-Signages', views.signages, name='signages'),
+    path('promotional-materials', views.promotional_materials, name='promotional_materials'),
+    path('banners-and-stickers', views.banners_stickers, name='banners_stickers'),
+    path('contact-us', views.contact, name='contact'),
+    
 ]
--- main/views.py
+++ main/views.py
@@ -7,3 +7,51 @@ def home(request):
         
     }    
     return render(request, template_name, context)
+
+def graphic_design(request):
+    template_name = 'main/graphic_design.html'
+    context = {
+        'title':"Graphic Design"    
+        
+    }    
+    return render(request, template_name, context)
+
+def tshirt_printing(request):
+    template_name = 'main/tshirt_printing.html'
+    context = {
+        'title':"Tshirt-Printing"    
+        
+    }    
+    return render(request, template_name, context)
+
+def signages(request):
+    template_name = 'main/signages.html'
+    context = {
+        'title':"2D & 3D Signages"    
+        
+    }    
+    return render(request, template_name, context)
+
+def banners_stickers(request):
+    template_name = 'main/banners_stickers.html'
+    context = {
+        'title':"Banners and Stickers"    
+        
+    }    
+    return render(request, template_name, context)
+
+def promotional_materials(request):
+    template_name = 'main/promotional_materials.html'
+    context = {
+        'title':"Home"    
+        
+    }    
+    return render(request, template_name, context)
+
+def contact(request):
+    template_name = 'main/contact.html'
+    context = {
+        'title':"Contacts"    
+        
+    }    
+    return render(request, template_name, context)
